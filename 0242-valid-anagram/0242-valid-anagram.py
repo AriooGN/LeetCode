b2757a453a8a9bf487d81fee1f4344524757f990
Arian <113949338+AriooGN@users.noreply.github.com>
2024-09-12 19:04:51 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,6 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if(len(s) != len(t)):
-            return False
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s.sort()
+        t.sort()
 
-        sortS = sorted(s)
-        sortT = sorted(t)
-
-        return sortS == sortT
-
-        
+        return s == t
