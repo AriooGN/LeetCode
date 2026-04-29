a2639c5e289c0f2aa52ab1b297376dbed320ef8e
Arian <113949338+AriooGN@users.noreply.github.com>
2026-04-29 09:54:41 -0400

    Time: 3 ms (76.36%), Space: 19.2 MB (95.03%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,34 +1,25 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        checkSet = set()
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        # Check rows
-        for i in range(len(board)):
-            checkSet.clear()  # Clear the set for each row
-            for j in range(len(board)):
-                if board[i][j] in checkSet and board[i][j] != '.':  # Check for duplicates
+        for i in range(9):
+            for j in range(9):
+                current_cell = board[i][j]
+
+                if current_cell == ".":
+                    continue
+
+
+                if( current_cell in rows[i] 
+                    or current_cell in cols[j] 
+                    or current_cell in squares[(i // 3, j // 3)] ):
                     return False
-                else:
-                    checkSet.add(board[i][j])  # Add current value to the set
-        
-        # Check columns
-        for i in range(len(board)):
-            checkSet.clear()  # Clear the set for each column
-            for j in range(len(board)):
-                if board[j][i] in checkSet and board[j][i] != '.':  # Check for duplicates
-                    return False
-                else:
-                    checkSet.add(board[j][i])  # Add current value to the set
-        
-        # Check subgrids (3x3 boxes)
-        for i in range(0, 9, 3):  # Iterate through rows of subgrids
-            for j in range(0, 9, 3):  # Iterate through columns of subgrids
-                checkSet.clear()  # Clear the set for each subgrid
-                for x in range(3):  # Iterate through rows within the subgrid
-                    for y in range(3):  # Iterate through columns within the subgrid
-                        if board[i+x][j+y] in checkSet and board[i+x][j+y] != '.':  # Check for duplicates
-                            return False
-                        else:
-                            checkSet.add(board[i+x][j+y])  # Add current value to the set
-        
-        return True  # If all checks pass, the Sudoku board is valid
+
+                    
+                cols[j].add(current_cell)
+                rows[i].add(current_cell)
+                squares[i // 3, j//3].add(current_cell)
+            
+        return True
